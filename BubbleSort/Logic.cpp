941ae6ed2e7a896262ba2458e214603a0ad6fa2b
Alejandro Zapata Molina <32116372+Alejandro2134@users.noreply.github.com>
2017-12-01 11:43:04 -0500

    Update Logic.cpp

--- BubbleSort/Logic.cpp
+++ BubbleSort/Logic.cpp
@@ -4,66 +4,72 @@
  *  Created on: 27/11/2017
  *      Author: Tato
  */
-#include <vector>
-#include <stdlib.h>
-#include <iostream>
-#include <string.h>
-using namespace std;
-
+#include <vector>  //Libreria para manejar vectores.
+#include <stdlib.h> //Libreria para manejar entradas y salidas.
+#include <iostream> //Libreria para manejar entradas y salidas.
+#include <string.h> //Libreria para manejar strings.
+using namespace std; //Declaración de namespace para hacer uso de palabras reservadas.
+/*
+*Esta función representa el comportamiento de bubblesort, esta recibe
+*como parametro un vector con numeros desordenados.
+*/
 void bubblesort(vector <int> A)
 {
-	bool swapped;
-	int swap;
-	int length = A.size();
+	bool swapped;	//Variable de tipo booleano la cual cambia dependiendo unas condiciones.
+	int swap; 	//Variable la cual almacenara un número para ser cambiado de posición.
+	int length = A.size(); //Variable la cual almacena el tamaño del vector pasado como parametro.
 
-	for(int i = 0; i < length; i++)
+	for(int i = 0; i < length; i++) //Ciclo que va desde 0 hasta el valor que tenga la variable length.	
 	{
-		swapped = false;
+		swapped = false; //Se inicializa la variable swapped en false.	
 
-		for(int j = 0; j < length; j++)
+		for(int j = 0; j < length; j++) //Ciclo que desde 0 hasta el valor que tenga la variable length.
 		{
-			if(A[j] > A[j+1])
+			if(A[j] > A[j+1]) //Condicional para conocer si la posición adyacente es mayor.
 			{
-				swap = A[j];
-				A[j] = A[j+1];
-				A[j+1] = swap;
-				swapped = true;
+				swap = A[j]; //Si la condición es true se almacenara la posición adyacente en la variable swap. 
+				A[j] = A[j+1]; //El valor de la posición adyacente cambia por el valor de la posición de su derecha.
+				A[j+1] = swap; //El valor de la derecha se cambia por el valor guardado en la variable swap y de esta forma se intercambian las posiciones.
+				swapped = true; //La variable swapped cambia su valor por true.
 			}
 
-			if(!swapped)
+			if(!swapped) //Condicional para controlar que el array ya este ordenado
 			{
-				break;
+				break;  //Si la condición es true se rompe primer ciclo. 
 			}
 		}
 
 	}
 
-	for(int i = 0; i < length; i++)
+	for(int i = 0; i < length; i++) //Ciclo que va desde 0 hasta el valor que tenga la variable length.
 	{
-		cout << A[i] << " ";
+		cout << A[i] << " "; //Se hace la impresión de el array ya ordenado.
 	}
 }
 
+/*
+*Función main el cual crea el vector y lo pasa como parametro a la función bubblesort.
+*/
 int main (){
 
-	vector <int> A;
-	int z = 0;
-	int tam = 0;
-	cout << "Cuantos numeros desea organizar?:";
-	cin >> tam;
-	char input[tam];
-	char* ptr;
-	cout << "Ingrese " << tam << " numeros desordenados y separados por comas:";
-	cin >> input;
-	ptr = strtok(input, ",");
+	vector <int> A; //Vector para almacenar numeros desordenados.
+	int z = 0; 	
+	int tam = 0; 	//Variable que guarda el tamaño de una cadena compuesta de numeros y comas,este va a ser pedido por consola.
+	cout << "Cuantos numeros desea organizar?:"; //Se pide el tamaño.
+	cin >> tam; //El tamaño se guarda en la variable tam.
+	char input[tam]; //Se crea una cadena de caracteres con un tamaño el cual esta guardado en la variable tam.
+	char* ptr; 
+	cout << "Ingrese " << tam << " numeros desordenados y separados por comas:"; // Se pide que se ingrese un char con datos separados por comas
+	cin >> input; //Se guarda en una variable.
+	ptr = strtok(input, ","); //Se llama la función strtok para que divida la la cadena cada vez que se encuentre una coma. 
 
 	while(ptr != NULL)
 	{
-		A[z++] = atoi(ptr);
-		ptr = strtok(NULL, ",");
+		A[z++] = atoi(ptr); //Cada char se comvierte en entero con la función atoi y se guarda en un vector.
+		ptr = strtok(NULL, ","); 
 	}
 
-	bubblesort(A);
+	bubblesort(A); //Se llama la función bubblesort pasandole como parametro el vector de numeros desorganizados.
 	return 0;
 
 }
